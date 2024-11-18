b881f135999598852ae35a94916ba5a5a032bd80
Alvaro Suaya <87244730+Godwit2110@users.noreply.github.com>
2024-11-18 10:58:15 -0300

    creation of files

--- ExchangeCoin-Front/src/app/app.module.ts
+++ ExchangeCoin-Front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscriptionComponent } from './pages/subscription/subscription.component';
+import { CoinsComponent } from './pages/admin/coins/coins.component';
+import { UsersComponent } from './pages/admin/users/users.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ExchangeComponent } from './pages/exchange/exchange.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscriptionComponent,
+    CoinsComponent,
+    UsersComponent,
+    LoginComponent,
+    RegisterComponent,
+    ExchangeComponent
   ],
   imports: [
     BrowserModule,
