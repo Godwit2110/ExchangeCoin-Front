410b2226583e52705bd243daab89ba53f5cd3a60
Alvaro Suaya <87244730+Godwit2110@users.noreply.github.com>
2024-11-18 22:32:19 -0300

    subscription done

--- ExchangeCoin-Front/src/app/pages/subscription/subscription.component.ts
+++ ExchangeCoin-Front/src/app/pages/subscription/subscription.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExchangeService } from 'src/app/services/exchange.api';
 
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
-  styleUrls: ['./subscription.component.scss']
+  styleUrls: ['./subscription.component.scss'],
 })
 export class SubscriptionComponent {
+  ExchangeService = inject(ExchangeService);
+  router = inject(Router);
 
+  ChangeSubscription(id: number) {
+    this.ExchangeService.ChangeSubscription(id);
+    this.router.navigate(['/exchange']);
+  }
 }
